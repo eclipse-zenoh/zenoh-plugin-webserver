7ef975e600f6f03bfd1098728e0a74f4d2cf0ad0
Julien Enoch <julien.enoch@adlinktech.com>
2020-12-11 17:27:19 +0100

    1st commit

---
+++ README.md
@@ -0,0 +1,72 @@
+<img src="http://zenoh.io/img/zenoh-dragon-small.png" width="150">
+
+[![CI](https://github.com/eclipse-zenoh/zenoh-plugin-webserver/workflows/CI/badge.svg)](https://github.com/eclipse-zenoh/zenoh-plugin-webserver/actions?query=workflow%3A%22CI%22)
+[![Gitter](https://badges.gitter.im/atolab/zenoh.svg)](https://gitter.im/atolab/zenoh?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge)
+[![License](https://img.shields.io/badge/License-EPL%202.0-blue)](https://choosealicense.com/licenses/epl-2.0/)
+[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+
+# Web Server plugin for Eclipse zenoh
+
+The Web Server plugin implements an HTTP server mapping URLs to zenoh paths.  
+This plugin can be used to set-up a Web server where the resources are retrieved from geo-distributed
+zenoh storages, each leveraging various backends (file system, database, memory...).
+
+**Library name** `zplugin_webserver`
+
+**Startup arguments** (added to zenoh router’s startup arguments):
+ - `--web-server-port=[PORT]`: The Web Server plugin's http port (default: 80)
+
+:point_right: **Download:** https://download.eclipse.org/zenoh/zenoh-plugin-webserver/
+
+-------------------------------
+## **Examples of usage**
+
+Assuming you have a static website stored in `/var/www/html` directory, you can:
+ - expose the files as zenoh key/values using the [File System backend](https://github.com/eclipse-zenoh/zenoh-backend-filesystem)
+ - set-up this Web Server plugin that will allow HTTP clients to browse the files.
+
+Here are the steps:
+ 1. Make sure the libraries for the File System backend and the Web Server plugin are available for the zenoh router:  
+    either installing their packages (depending your platform), either downloading the library files corresponding
+    to your platform in your `~/.zenoh/lib` directory.
+ 2. Start the zenoh router (`zenohd`). It will automatically load the Web Server plugin and make it available on port 80.
+ 3. Add the File System backend (for instance using `curl` on the REST API):
+    ```bash
+    curl -X PUT http://localhost:8000/@/router/local/plugin/storages/backend/fs
+    ```
+ 4. Add a File System storage exposing the `/var/www/html` directory in read-only mode under the `/my-site` zenoh prefix:
+    ```bash
+    curl -X PUT -H 'content-type:application/properties' -d "path_expr=/my-site/**;path_prefix=/my-site;dir=/var/www/html;read_only" http://localhost:8000/@/router/local/plugin/storages/backend/fs/storage/my-site
+    ```
+ 5. Now you can browse your site on http://localhost/my-site.
+
+
+For more advanced use cases you can also:
+ - Have the files of your web sites stored on different hosts. Running a zenoh router with a File System Storage on
+   each host allow to make all the files available under the `/my-site` zenoh path.
+ - Duplicate the files of your web sites on several hosts to provide fault tolerance.
+ - Start several zenoh routers with the Web Service plugin on different hosts (not necessarly the same than the
+   hosts running the File System storages). Each host will serve your web site.
+ - Use other backends than the File System to store your resources and make them available through zenoh.
+   (List of available backends [here](http://zenoh.io/docs/manual/backends-list/)).
+ - Deploy a zenoh application that will implement `eval` function for a resource, replying to requests with a
+   dynamic content (see the `z_eval` example in
+   [Rust](https://github.com/eclipse-zenoh/zenoh/blob/master/zenoh/examples/zenoh/z_eval.rs) or
+   [Python](https://github.com/eclipse-zenoh/zenoh-python/blob/master/examples/zenoh/z_eval.py)).
+
+
+
+-------------------------------
+## How to build it
+
+Install [Cargo and Rust](https://doc.rust-lang.org/cargo/getting-started/installation.html). Currently, zenoh requires a nightly version of Rust, type the following to install it after you have followed the previous instructions:
+
+```bash
+$ rustup default nightly
+```
+
+And then build the backend with:
+
+```bash
+$ cargo build --release --all-targets
+```
---
+++ src/lib.rs
@@ -0,0 +1,198 @@
+//
+// Copyright (c) 2017, 2020 ADLINK Technology Inc.
+//
+// This program and the accompanying materials are made available under the
+// terms of the Eclipse Public License 2.0 which is available at
+// http://www.eclipse.org/legal/epl-2.0, or the Apache License, Version 2.0
+// which is available at https://www.apache.org/licenses/LICENSE-2.0.
+//
+// SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
+//
+// Contributors:
+//   ADLINK zenoh team, <zenoh@adlink-labs.tech>
+//
+#![feature(async_closure)]
+
+use async_std::sync::Arc;
+use clap::{Arg, ArgMatches};
+use futures::prelude::*;
+use std::convert::TryFrom;
+use std::str::FromStr;
+use tide::http::Mime;
+use tide::{Request, Response, Server, StatusCode};
+use zenoh::net::*;
+use zenoh::{PathExpr, Selector, Value, Workspace, Zenoh};
+use zenoh_protocol::io::RBuf;
+use zenoh_router::runtime::Runtime;
+
+const PORT_SEPARATOR: char = ':';
+const DEFAULT_HTTP_HOST: &str = "0.0.0.0";
+const DEFAULT_HTTP_PORT: &str = "80";
+const DEFAULT_DIRECTORY_INDEX: &str = "index.html";
+
+lazy_static::lazy_static! {
+    static ref DEFAULT_MIME: Mime = encoding::to_mime(encoding::APP_OCTET_STREAM).unwrap();
+}
+
+#[no_mangle]
+pub fn get_expected_args<'a, 'b>() -> Vec<Arg<'a, 'b>> {
+    vec![
+        Arg::from_usage("--web-server-port 'The Web Server plugin's http port'")
+            .default_value(DEFAULT_HTTP_PORT),
+    ]
+}
+
+#[no_mangle]
+pub fn start(runtime: Runtime, args: &'static ArgMatches<'_>) {
+    async_std::task::spawn(run(runtime, args));
+}
+
+async fn run(runtime: Runtime, args: &'static ArgMatches<'_>) {
+    env_logger::init();
+
+    let http_port = parse_http_port(args.value_of("web-server-port").unwrap());
+
+    let zenoh = Zenoh::init(runtime).await;
+
+    let mut app = Server::with_state(Arc::new(zenoh));
+
+    app.at("*").get(async move |req: Request<Arc<Zenoh>>| {
+        // Reconstruct Selector from req.url() (no easier way...)
+        let url = req.url();
+        log::debug!("GET on {}", url);
+
+        // Build corresponding Selector
+        let mut s = String::with_capacity(url.as_str().len());
+        s.push_str(url.path());
+
+        // if URL id a directory, append DirectoryIndex
+        if s.ends_with('/') {
+            s.push_str(DEFAULT_DIRECTORY_INDEX);
+        }
+
+        let workspace = req.state().workspace(None).await.unwrap();
+        if let Some(q) = url.query() {
+            s.push('?');
+            s.push_str(q);
+        }
+        if let Some(f) = url.fragment() {
+            s.push('#');
+            s.push_str(f);
+        }
+        let mut selector = match Selector::try_from(s) {
+            Ok(sel) => sel,
+            Err(e) => {
+                return Ok(bad_request(
+                    &e.to_string(),
+                ))
+            }
+        };
+        log::trace!("GET on {} => selector: {}", url, selector);
+
+        // Check if selector's path expression is a Path (i.e. for a single resource)
+        if !selector.path_expr.is_a_path() {
+            return Ok(bad_request(
+                "The URL must correspond to 1 resource only (i.e. zenoh path expressions not supported)",
+            ))
+        }
+
+        match zenoh_get(&workspace, &selector).await {
+            Ok(Some(value)) => Ok(response_with_value(value)),
+            Ok(None) => {
+                // Check if considering the URL as a directory, there is an existing "URL/DirectoryIndex" resource
+                selector.path_expr = PathExpr::new(format!("{}/{}", selector.path_expr.as_str(), DEFAULT_DIRECTORY_INDEX)).unwrap();
+                if let Ok(Some(_)) = zenoh_get(&workspace, &selector).await {
+                    // In this case, we must reply a redirection to the URL as a directory
+                    Ok(redirect(&format!("{}/", url.path())))
+                } else {
+                    Ok(not_found())
+                }
+            },
+            Err(e) => Ok(internal_error(
+                &e.to_string(),
+            ))
+        }
+    });
+
+    if let Err(e) = app.listen(http_port).await {
+        log::error!("Unable to start http server for REST : {:?}", e);
+    }
+}
+
+fn parse_http_port(arg: &str) -> String {
+    match arg.split(':').count() {
+        1 => {
+            match arg.parse::<u16>() {
+                Ok(_) => [DEFAULT_HTTP_HOST, arg].join(&PORT_SEPARATOR.to_string()), // port only
+                Err(_) => [arg, DEFAULT_HTTP_PORT].join(&PORT_SEPARATOR.to_string()), // host only
+            }
+        }
+        _ => arg.to_string(),
+    }
+}
+
+async fn zenoh_get(workspace: &Workspace<'_>, selector: &Selector) -> ZResult<Option<Value>> {
+    let mut stream = workspace.get(&selector).await?;
+    Ok(stream.next().await.map(|data| data.value))
+}
+
+fn response_with_value(value: Value) -> Response {
+    match value {
+        Value::Custom {
+            encoding_descr,
+            data,
+        } => {
+            log::debug!("Replying with a Custom Value ({})", encoding_descr);
+            response_ok(
+                Mime::from_str(&encoding_descr).unwrap_or_else(|_| DEFAULT_MIME.clone()),
+                data,
+            )
+        }
+        Value::Raw(encoding, data) => {
+            log::debug!("Replying with a Raw Value ({})", encoding);
+            response_ok(
+                encoding::to_mime(encoding).unwrap_or_else(|_| DEFAULT_MIME.clone()),
+                data,
+            )
+        }
+        _ => {
+            let (encoding, data) = value.encode();
+            log::debug!("Replying with a decoded Value ({})", encoding);
+            response_ok(
+                encoding::to_mime(encoding).unwrap_or_else(|_| DEFAULT_MIME.clone()),
+                data,
+            )
+        }
+    }
+}
+
+fn bad_request(body: &str) -> Response {
+    let mut res = Response::new(StatusCode::BadRequest);
+    res.set_content_type(Mime::from_str("text/plain").unwrap());
+    res.set_body(body);
+    res
+}
+
+fn not_found() -> Response {
+    Response::new(StatusCode::NotFound)
+}
+
+fn internal_error(body: &str) -> Response {
+    let mut res = Response::new(StatusCode::InternalServerError);
+    res.set_content_type(Mime::from_str("text/plain").unwrap());
+    res.set_body(body);
+    res
+}
+
+fn redirect(url: &str) -> Response {
+    let mut res = Response::new(StatusCode::MovedPermanently);
+    res.insert_header("Location", url);
+    res
+}
+
+fn response_ok(content_type: Mime, payload: RBuf) -> Response {
+    let mut res = Response::new(StatusCode::Ok);
+    res.set_content_type(content_type);
+    res.set_body(payload.get_vec());
+    res
+}
